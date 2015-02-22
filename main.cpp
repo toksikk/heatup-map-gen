56f6e8f96cebb14d167188e65eaf4194ce45ecaf
toksikk <sykikk+github@gmail.com>
2015-02-22 18:24:47 +0100

    Implemented simple algorithm Way pattern is generated and 1 block sized hardware tiles.

--- main.cpp
+++ main.cpp
@@ -9,8 +9,9 @@ int main(void) {
 	int tries = 0;
 	int hwtotaltiles = 0;
 	
-	// folgende for-schleife setzt hardware-gametiles
-	for (int i = 0; i < 10; i++) {
+	// the following algorithm sets hardware blocks but no ways.
+	// for the purpose if somebody wants to implement an algorithm to generate ways along existing hardware.
+/*	for (int i = 0; i < 9; i++) {
 		hw++;
 
 		int minimumsize = 2;	
@@ -47,13 +48,40 @@ int main(void) {
 			}
 		}
 	}
+*/
+	// algorithm generates a way-pattern and afterwards sets 9 1 block sized manipulable gametiles.
+	int waymapcoveringgoal = 100;
 
-	for (int i=0; i< (int)((32*24-hwtotaltiles)*0.5); i++) {
-		
+	for (int i=0; i< (32*24/100.0)*waymapcoveringgoal; i++) {
+		int posx = get_random_number(32);
+		int posy = get_random_number(24);
+		if (i==0) gm->getTile(posx,posy)->setWay();
+		while (check_bordering_way_amount(gm, posx, posy) != 1) {
+			tries++;
+			posx = get_random_number(32);
+			posy = get_random_number(24);
+			while (gm->getTile(posx,posy)->getChar() == '#') {
+				posx = get_random_number(32);
+				posy = get_random_number(24);
+				if (tries > 100000) break;
+			}
+			if (tries > 100000) break;
+		}
+		if (tries > 100000) break;
+		gm->getTile(posx,posy)->setWay();
 	}
 
-	//std::cout << check_surroundings_for_hw_to_e_s(gm, 4, 2, 1) << std::endl; // "1" == speicherzugriffsfehler
+	for (int i=0; i<9; i++) {
+		hw++;
+		int posx = get_random_number(32);
+		int posy = get_random_number(24);
 
+		while (gm->getTile(posx, posy)->getChar() != '-') {
+			posx = get_random_number(32);
+			posy = get_random_number(24);
+		}
+		gm->getTile(posx, posy)->setHardware(i);
+	}
 
 	gm->printMap();
 	delete gm;
